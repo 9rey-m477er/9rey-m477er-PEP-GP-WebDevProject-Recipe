aa8e481e6f591a0cf1f13693476a628ad097ac57
9rey-m477er <9rey-m477er@github.com>
2026-08-13 20:22:53 +0000

    Test run: 126/127 passed, 1 failed, 0 skipped - 2026-08-13T20:22:53.469Z

--- src/main/resources/public/frontend/recipe/recipe-page.js
+++ src/main/resources/public/frontend/recipe/recipe-page.js
@@ -217,7 +217,8 @@ window.addEventListener("DOMContentLoaded", () => {
             }
             const response = await fetch(`${BASE_URL}/recipes/${id}`, responseOptions);
             recipeDeleteInput.value = "";
-            refreshRecipeList();
+            getRecipes();
+            alert("Recipe deleted");
         } catch(error) {
             console.log(error);
             alert("An error occurred");
